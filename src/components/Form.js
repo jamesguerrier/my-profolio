dd7419a170d7d318707a7c3948f508b0cc02a8c9
James Guerrier <jamesguerrier72@gmail.com>
2023-09-04 16:39:27 -0500

    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,11 +7,11 @@ const Form = () => {
     <div className="form">
         <form>
             <label>Your Name</label>
-            <input type="text"></input> 
+            <input type="text" name="name"></input> 
             <label>Your Email</label>
-            <input type="email"></input>
+            <input type="email" name="email"></input>
             <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" name=" text"></input>
             <label>Message</label>
             <textarea name="" id="" cols="30" rows="10" placeholder="type your message here" />
             <button className="btn">Submit</button>
